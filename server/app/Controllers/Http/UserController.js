7d093082108fe0f13c147b10549853463f9a0e83
EZPK <thom.bruyer@gmail.com>
2018-05-31 14:40:03 +0200

    Finished API

--- server/app/Controllers/Http/UserController.js
+++ server/app/Controllers/Http/UserController.js
@@ -7,7 +7,7 @@ class UserController {
     async login({ request, auth }){
         const { email, password } = request.all()
         const token = await auth.attempt(email, password)
-
+        
         return token
     }
 
