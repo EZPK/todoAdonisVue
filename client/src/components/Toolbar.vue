d3b8f12e3151042624ac86e55c0f197d00c7432b
EZPK <thom.bruyer@gmail.com>
2018-06-04 16:50:16 +0200

    registration

--- client/src/components/Toolbar.vue
+++ client/src/components/Toolbar.vue
@@ -2,22 +2,22 @@
   <v-toolbar color="blue" dark fixed>
     <v-toolbar-title class="mr-5">ALGO</v-toolbar-title>
     <v-toolbar-items>
-      <v-btn flat>
+      <v-btn flat v-if="isLoggedIn">
       <v-icon>playlist_add_check</v-icon>
       Projects
       </v-btn>
     </v-toolbar-items>
     <v-spacer></v-spacer>
     <v-toolbar-items class="hidden-sm-and-down">
-      <v-btn flat to="/register">
+      <v-btn flat to="/register" v-if="!isLoggedIn">
         <v-icon class="mr-2">account_box</v-icon>
         Register
       </v-btn>
-      <v-btn flat>
+      <v-btn flat v-if="!isLoggedIn">
         <v-icon class="mr-2">fingerprint</v-icon>
         Login
       </v-btn>
-      <v-btn flat>
+      <v-btn flat v-if="isLoggedIn">
         <v-icon class="mr-2">exit_to_app</v-icon>
         Logout
       </v-btn>
@@ -26,7 +26,15 @@
 </template>
 
 <script>
-export default {};
+import { mapGetters } from 'vuex';
+
+export default {
+  computed: {
+    ...mapGetters('authentication', [
+      'isLoggedIn',
+    ]),
+  },
+};
 </script>
 
 <style></style>
